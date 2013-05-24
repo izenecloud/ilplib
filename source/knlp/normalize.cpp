74d76b7435cd3dd9dbd017fbab12005354d966ed
Kevin Hu <kevin.hu@b5m.com>
2013-05-24 11:41:36 +0800

    remove warning

---
+++ source/knlp/normalize.cpp
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  normalize.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2013年05月24日 11时07分41秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Kevin Hu (), kevin.hu@b5m.com
+ *        Company:  B5M.com
+ *
+ * =====================================================================================
+ */
+#include "knlp/normalize.h"
+namespace ilplib{
+	    namespace knlp{
+
+Trad2Simp Normalize::trd2smp_;
+
+}}
+

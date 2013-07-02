95ac60aa3652b51ba92705aabb37313f3ea7af15
Jun Jiang <jun.jiang@izenesoft.com>
2013-07-02 11:10:31 +0800

    According to adjust in ilplib::knlp::Tokenize, revise t_knlp_tokenize below:
    for token not exist in dictionary, if it contains no more than 3 Chinese
    characters, then it would be assumed as one token.

--- test/knlp/t_knlp_tokenize.cpp
+++ test/knlp/t_knlp_tokenize.cpp
@@ -28,17 +28,19 @@ BOOST_AUTO_TEST_CASE(testFMM)
 {
     init(kDictContent);
 
+    // for token not exist in dictionary, if it contains no more than 3 Chinese
+    // characters, then it would be assumed as one token
     checkFMM("APPLE/苹果 IPHONE (16GB)   电信版 手机",
-             "apple / 苹果 iphone ( 16gb ) 电 信 版 手机");
+             "apple / 苹果 iphone ( 16gb ) 电信版 手机");
 
     checkFMM("HTC T329d 3G手机(黑色)CDMA2000/GSM 【双模双待双通】",
-             "htc t329d 3g 手机 ( 黑 色 ) cdma2000 / gsm 【 双模双待 双 通 】");
+             "htc t329d 3g 手机 ( 黑色 ) cdma2000 / gsm 【 双模双待 双 通 】");
 
     checkFMM("三星 S7562I 3G手机(纯白)WCDMA/GSM 双卡双待",
-             "三星 s7562i 3g 手机 ( 纯 白 ) wcdma / gsm 双 卡 双 待");
+             "三星 s7562i 3g 手机 ( 纯白 ) wcdma / gsm 双 卡 双 待");
 
     checkFMM("艾米 AMII 宽肩立领风衣外套C1NC5121137",
-             "艾 米 amii 宽肩立领风衣外套 c1nc5121137");
+             "艾米 amii 宽肩立领风衣外套 c1nc5121137");
 }
 
 BOOST_AUTO_TEST_SUITE_END()

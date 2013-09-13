b8daf08ad5ddb0ce2c87be5eb8e1e2530500a15d
Kevin Hu <kevin.hu@b5m.com>
2013-09-13 13:55:33 +0800

    tiny

--- test/knlp/t_load_resource.cpp
+++ test/knlp/t_load_resource.cpp
@@ -53,18 +53,18 @@ cout<<"dict ok"<<endl;
     BOOST_CHECK(c_dict.cate_trans(KString("A1")) == (size_t)0);
     BOOST_CHECK(c_dict.get_score(c_dict.cate_trans(KString("B 4"))) == 5);
     BOOST_CHECK(c_dict.get_score(c_dict.cate_trans(KString("a1>A1"))) == 6);
-    BOOST_CHECK(c_dict.get_score(-1) == NOT_FOUND);
+    BOOST_CHECK(c_dict.get_score(-1) == c_dict.NOT_FOUND_);
 
     BOOST_CHECK(dict.size() == 6);  
     BOOST_CHECK(dict.token_trans(KString("abc")) == (size_t)-1);
     BOOST_CHECK(dict.token_trans(KString("1 0")) == (size_t)0);
     BOOST_CHECK(dict.size(dict.token_trans(KString("1 0"))) == 4);
     BOOST_CHECK(dict.size(dict.token_trans(KString("b"))) == 0); 
-    BOOST_CHECK(dict.size(dict.token_trans(KString("w"))) == NOT_FOUND);
+    BOOST_CHECK(dict.size(dict.token_trans(KString("w"))) == c_dict.NOT_FOUND_);
     BOOST_CHECK(dict.size(dict.token_trans(KString("a b c"))) == 3);
     BOOST_CHECK(dict.get_score(dict.token_trans(KString("1 0")), 0) == 1.1);
     BOOST_CHECK(dict.get_score(dict.token_trans(KString("a")), 2) == 3.3);
-    BOOST_CHECK(dict.get_score(4, 111133) == NOT_FOUND);
+    BOOST_CHECK(dict.get_score(4, 111133) == c_dict.NOT_FOUND_);
     
     BOOST_CHECK(c_dict.cate_trans(dict.get_ind(dict.token_trans(KString("1 0")), 0)) == KString("a1"));
     BOOST_CHECK(c_dict.cate_trans(dict.get_ind(dict.token_trans(KString("aaaa")), 4)) == KString("B 4"));

9c5a689e7f0360da4829f94ab9016c5b835b094d
B5M-D23 <B5MOPS@b5m.com>
2013-12-16 15:45:11 +0800

    add new tokenizer for the comming horse year

---
+++ include/knlp/horse_tokenize.h
@@ -0,0 +1,99 @@
+#ifndef _ILPLIB_NLP_HORSE_TOKENIZE_H_
+#define _ILPLIB_NLP_HORSE_TOKENIZE_H_
+
+#include <string>
+#include <vector>
+#include <list>
+#include <fstream>
+#include <cstdlib>
+#include <algorithm>
+#include <istream>
+#include <ostream>
+#include <cctype>
+#include <math.h>
+
+#include "util/string/kstring.hpp"
+#include "normalize.h"
+#include "kdictionary.h"
+#include "am/util/line_reader.h"
+
+namespace ilplib
+{
+namespace knlp
+{
+class HorseTokenize{
+    KDictionary<float> tk_dict_;
+    KDictionary<const char*> rewrite_dict_;
+
+    bool is_digit_(char c)const
+    {
+        if (c >= '0' && c<='9')
+            return true;
+        if (c == '.' || c == '-' || c == '+' || c == '/' || c == '=' || c== '*' || c== '%'
+          || c == ',' || c == '$' || c == '&')
+            return true;
+        return false;
+    }
+
+    void merge_(std::vector<std::pair<std::string, float> >& tks)const
+    {
+        std::vector<bool> flags(tks.size(), 0);
+        for (uint32_t i=0;i<tks.size();i++)
+        {
+            uint32_t j=0;
+            for (;j<tks[i].first.length();j++)
+                if (is_digit_(tks[i].first[j]))
+                    break;
+            if (j < tks[i].first.length())flags[i] = true;
+        }
+
+        for (uint32_t i=0;i<tks.size();i++)if(flags[i])
+        {
+            uint32_t t = i;
+            i++;
+            while(i<tks.size() && flags[i])
+            {
+                tks[t].first += tks[i].first;
+                tks[t].second += tks[i].second;
+                tks.erase(tks.begin()+i);
+            }
+        }
+    }
+
+    void rewrite_(std::vector<std::pair<std::string, float> >& tks)const
+    {
+        for (uint32_t i=0;i<tks.size();i++)
+        {
+            const char* p = NULL;
+            if (rewrite_dict_.value(tks[i].first, p, false)!=0)
+                continue;
+            assert(p != NULL);
+            tks[i].first = std::string(p);
+        }
+        for (uint32_t i=0;i<tks.size();i++)
+            if (tks[i].first == " ")
+            {
+                tks.erase(tks.begin()+i);
+                i--;
+            }
+    }
+
+public:
+    HorseTokenize(const std::string& dir)
+      :tk_dict_(dir+"/token.dict")
+       ,rewrite_dict_(dir + "rewrite.dict")
+    {
+    }
+
+    void tokenize(std::string line, 
+      std::vector<std::pair<std::string, float> >& tks)const
+    {
+        tk_dict_.fmm(line, tks);
+        merge_(tks);
+        rewrite_(tks);
+    }
+};
+}
+}
+
+#endif
--- include/knlp/normalize.h
+++ include/knlp/normalize.h
@@ -45,8 +45,66 @@ public:
     {
         izenelib::util::KString kstr(str);
         normalize(kstr);
-        str = kstr.get_bytes("utf-8");
+        str = unicode_to_utf8(kstr);
     }
+
+    static std::string unicode_to_utf8(const izenelib::util::KString& kstr)
+    {
+        std::string s;
+        s.reserve(kstr.length() << 2);
+        for(size_t i = 0; i < kstr.length(); ++i)
+        {
+            uint16_t unic = kstr[i];
+            if ( unic <= 0x0000007F )
+            {
+                // * U-00000000 - U-0000007F:  0xxxxxxx
+                s.append(1, unic & 0x7F);
+            }
+            else if ( unic >= 0x00000080 && unic <= 0x000007FF )
+            {
+                // * U-00000080 - U-000007FF:  110xxxxx 10xxxxxx
+                s.append(1, (((unic >> 6) & 0x1F) | 0xC0));
+                s.append(1, ((unic & 0x3F) | 0x80));
+            }
+            else if ( unic >= 0x00000800 && unic <= 0x0000FFFF )
+            {
+                // * U-00000800 - U-0000FFFF:  1110xxxx 10xxxxxx 10xxxxxx
+                s.append(1, (((unic >> 12) & 0x0F) | 0xE0));
+                s.append(1, (((unic >>  6) & 0x3F) | 0x80));
+                s.append(1, ((unic & 0x3F) | 0x80));
+            }
+            else if ( unic >= 0x00010000 && unic <= 0x001FFFFF )
+            {
+                // * U-00010000 - U-001FFFFF:  11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+                s.append(1, (((unic >> 18) & 0x07) | 0xF0));
+                s.append(1, (((unic >> 12) & 0x3F) | 0x80));
+                s.append(1, (((unic >>  6) & 0x3F) | 0x80));
+                s.append(1, ((unic & 0x3F) | 0x80));
+            }
+            else if ( unic >= 0x00200000 && unic <= 0x03FFFFFF )
+            {
+                // * U-00200000 - U-03FFFFFF:  111110xx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx
+                s.append(1, (((unic >> 24) & 0x03) | 0xF8));
+                s.append(1, (((unic >> 18) & 0x3F) | 0x80));
+                s.append(1, (((unic >> 12) & 0x3F) | 0x80));
+                s.append(1, (((unic >>  6) & 0x3F) | 0x80));
+                s.append(1, ((unic & 0x3F) | 0x80));
+            }
+            else if ( unic >= 0x04000000 && unic <= 0x7FFFFFFF )
+            {
+                // * U-04000000 - U-7FFFFFFF:  1111110x 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx
+                s.append(1, (((unic >> 30) & 0x01) | 0xFC));
+                s.append(1, (((unic >> 24) & 0x3F) | 0x80));
+                s.append(1, (((unic >> 18) & 0x3F) | 0x80));
+                s.append(1, (((unic >> 12) & 0x3F) | 0x80));
+                s.append(1, (((unic >>  6) & 0x3F) | 0x80));
+                s.append(1, ((unic & 0x3F) | 0x80));
+            }
+        }
+        return s;
+    }
+
+
 };
 
 }

4e63270e3df422551a55f00fd8af587176b82b23
B5M-D23 <B5MOPS@b5m.com>
2014-04-29 17:03:19 +0800

    add keyword conditions

---
+++ include/knlp/keyword_condition.h
@@ -0,0 +1,141 @@
+#ifndef _ILPLIB_NLP_KEYWORD_CONDITION_H_
+#define _ILPLIB_NLP_KEYWORD_CONDITION_H_
+
+#include <string>
+#include <vector>
+#include <list>
+#include <fstream>
+#include <cstdlib>
+#include <algorithm>
+#include <istream>
+#include <ostream>
+#include <cctype>
+#include <math.h>
+#include <ctime>
+#include <boost/algorithm/string.hpp>
+
+#include "util/string/kstring.hpp"
+#include "normalize.h"
+#include "kdictionary.h"
+#include "am/util/line_reader.h"
+
+namespace ilplib
+{
+namespace knlp
+{
+class KeywordCondition{
+    KDictionary<const char*> price_dict_;
+    KDictionary<const char*> bigram_dict_;
+    KDictionary<const char*> unigram_dict_;
+    KDictionary<const char*> cate_dict_;
+    KDictionary<const char*> compare_dict_;
+
+    std::string static_condition_()
+    {
+        std::string cmt = "{\"property\":\"CommentCount\",\"operator\":\">\",\"value\":[10]},";
+        time_t t = time(NULL)-3600*24*5;   
+        char buf[255];memset(buf, 0, sizeof(buf));
+        strftime(buf, 255, "%Y%m%dT%H%M%S", localtime(&t)); 
+        std::string dt = std::string("{\"property\":\"DATE\",\"operator\":\">=\",\"value\":[")+buf+"]}";
+        return cmt+dt;
+    }
+
+    std::vector<std::string> lookup_(const std::string& kw, KDictionary<const char*>* dict)
+    {
+        std::vector<std::string> r;
+        const char* p = NULL;
+        if (dict->value(kw, p, false)==0){
+            assert(p);
+            std::string strp(p);
+            const char* t = strchr(strp.c_str(), '\t');
+            while(t)
+            {
+                r.push_back(strp.substr(0, t-strp.c_str()));
+                strp = t+1;
+                t = strchr(strp.c_str(), '\t');
+            }
+            if (strp.length() > 0)
+                r.push_back(strp);
+        }
+        return r;
+    }
+
+    void combination_(const std::vector<std::vector<std::string> >& conds, std::string comb, 
+      std::vector<std::string>& r, uint32_t level=0)
+    {
+        if (level >= conds.size())
+        {
+            r.push_back(comb);return;
+        }
+
+        for (uint32_t i=0;i<conds[level].size();i++)if(conds[level][i].length()>0)
+            combination_(conds, comb+","+conds[level][i], r, level+1);
+    }
+
+    std::string normalize_(const std::string& q)
+    {
+        std::string kw;
+        for (uint32_t i=0;i<q.length();i++)
+            if (q[i]>='A' && q[i]<='Z')kw += char(int(q[i])-(int('A')-int('a')));
+            else if(q[i]!=' ')kw += q[i];
+        return kw;
+    }
+
+public:
+    KeywordCondition(const std::string& dir)
+      :price_dict_(dir+"/price.dict")
+       ,bigram_dict_(dir + "/bigram.dict")
+       ,unigram_dict_(dir + "/unigram.dict")
+       ,cate_dict_(dir + "/cate.dict")
+       ,compare_dict_(dir + "/compare.dict")
+    {
+    }
+
+    std::string compare_price(std::string kw, bool& compare_first)
+    {
+          kw = normalize_(kw);
+          compare_first = false;
+          std::vector<std::string> v = lookup_(kw, &compare_dict_);
+          if (v.size() > 0)compare_first = true;
+          if (v.size() > 0)return "{\"property\":\"itemcount\",\"operator\":\">\",\"value\":[1]}";
+          return "{\"property\":\"itemcount\",\"operator\":\"=\",\"value\":[1]}";
+    }
+
+    std::vector<std::pair<std::string, std::string> >
+      conditions(std::string kw)
+      {
+          kw = normalize_(kw);
+
+          std::vector<std::string> v = lookup_(kw, &bigram_dict_);
+          for (uint32_t i=0;i<v.size();i++)v[i]=kw+" "+v[i];
+          std::vector<std::string> exp(v.begin(), v.end());
+          v = lookup_(kw, &unigram_dict_);
+          for (uint32_t i=0;i<v.size();i++)v[i]=kw+" "+v[i];
+          exp.insert(exp.end(), v.begin(), v.end());
+          exp.push_back(kw);
+
+          std::vector<std::vector<std::string> > conds;
+          v = lookup_(kw, &price_dict_);
+          for (uint32_t i=0;i<v.size();i++)
+              v[i] = std::string("{\"property\":\"Price\",\"operator\":\">=\",\"value\":[")+v[i]+"]}";
+          if (v.size()) conds.push_back(v);
+
+          v = lookup_(kw, &cate_dict_);
+          for (uint32_t i=0;i<v.size();i++)
+              v[i] = std::string("{\"property\":\"Category\",\"operator\":\"starts_with\",\"value\":[")+v[i]+"]}";
+          if (v.size()) conds.push_back(v);
+
+          std::vector<std::string> comb;
+          combination_(conds, static_condition_(), comb);
+          std::vector<std::pair<std::string, std::string> > r;
+
+          for (uint32_t i=0;i<exp.size();i++)
+              for(uint32_t j=0;j<comb.size();j++)
+                  r.push_back(std::make_pair(exp[i], comb[j]));
+          return r;
+      }
+};
+}
+}
+
+#endif
---
+++ test/knlp/kw_condition_util.cpp
@@ -0,0 +1,70 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  tokenize_util.cc
+ *
+ *    Description:
+ *
+ *        Version:  1.0
+ *        Created:  01/16/2013 11:10:30 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Kevin Hu (), kevin.hu@b5m.com
+ *        Company:  iZeneSoft.com
+ *
+ * =====================================================================================
+ */
+#include <string>
+#include <algorithm>
+#include<iostream>
+#include<fstream>
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "knlp/keyword_condition.h"
+#include "knlp/fmm.h"
+#include "knlp/string_patterns.h"
+
+using namespace std;
+using namespace ilplib::knlp;
+using namespace izenelib;
+void printHelp()
+{
+    cout<< "./kw_condition_util\n"<<"\t-d\t resource path\n";
+}
+
+int main(int argc,char * argv[])
+{
+    if (argc == 1)
+    {
+        printHelp();
+        return 0;
+    }
+
+    char c = '?';
+    string dictnm;
+    while ((c = getopt (argc, argv, "d:")) != -1)
+        switch (c)
+        {
+        case 'd':
+            dictnm = optarg;
+            break;
+        case '?':
+            printHelp();
+        }
+
+    KeywordCondition kc(dictnm);
+    std::string line;
+    while(!std::getline(std::cin, line).eof())
+    {
+        std::cout<<line<<"SSssss\n";
+        std::vector<std::pair<std::string, std::string> > conds = kc.conditions(line);
+        for (uint32_t i=0;i<conds.size();i++)
+            std::cout<<conds[i].first<<"\t"<<conds[i].second<<std::endl;
+    }
+
+    return 0;
+}

ac770054325c6c0cd07a0b29c4c8e2bc72afbff0
Lehou <lehou@b5m.com>
2014-05-27 16:36:38 +0800

    update the program of getting tags in title

--- test/knlp/get_tags_util.cpp
+++ test/knlp/get_tags_util.cpp
@@ -8,21 +8,16 @@
 
 #include "knlp/get_tags.h"
 
-using namespace std;
 using namespace ilplib::knlp;
 using namespace izenelib;
 
 
 void printHelp()
 {
-    cout << "./get_tags_util\n" << "        \t-d\t dictionary path\n \
-        \t-p\ttitle tag frequency file\n\
-        \t-q\tquery tag frequency file\n\
+    std::cout << "./get_tags_util\n" << "        \t-d\t dictionary path\n \
         \t-n\ttop_n tags\n\
         \t-c\tcorpus path\n\
-        \t-o\toutput file\n";
-    cout << "  eg.: ./get_tags_util -d ../dict -p ../dict/title_tag_freq -q ../dict/query_tag_freq \
- -n 5 -c ../dict/test_corpus.txt  -o ../dict/test_result.txt" << endl;
+        \t-o\toutput file" << std::endl;
 }
 
 
@@ -35,22 +30,16 @@ int main(int argc,char * argv[])
     }
 
     char c = '?';
-    string dictnm;
-    string title_tag_freq;
-    string query_tag_freq;
+    std::string dictnm;
+    std::string title_tag_freq;
+    std::string query_tag_freq;
     unsigned int top_n = 10;
-    string corpus;
-    string output;
-    while ((c = getopt (argc, argv, "d:p:q:n:c:o:")) != -1) {
+    std::string corpus;
+    std::string output;
+    while ((c = getopt (argc, argv, "d:n:c:o:")) != -1) {
         switch (c) {
             case 'd':
                 dictnm = optarg;
-                break;
-            case 'p':
-                title_tag_freq = optarg;
-                break;
-            case 'q':
-                query_tag_freq = optarg;
                 break;
             case 'n':
                 top_n = atoi(optarg);
@@ -66,25 +55,25 @@ int main(int argc,char * argv[])
         }
     }
     
-    GetTags gt(dictnm, title_tag_freq, query_tag_freq);
-    ifstream fin(corpus.c_str());
+    GetTags gt(dictnm);
+    std::ifstream fin(corpus.c_str());
     if (!fin) {
-        cout << "in main(), failed to open the file: " << corpus << endl;
+        std::cout << "in main(), failed to open the file: " << corpus << std::endl;
         return 0;
     }
     
-    ofstream fout(output.c_str());
+    std::ofstream fout(output.c_str());
     if (!fout) {
-        cout << "in main(), failed to open the file: " << output << endl;
+        std::cout << "in main(), failed to open the file: " << output << std::endl;
         return 0;
     }
-
-    string line;
+    
+    std::string line;
     while (getline(fin, line)) {
         if (line.empty())
             continue;
         
-        vector<string> tags = gt.get_tags(line, top_n);
+        std::vector<std::string> tags = gt.get_tags(line, top_n);
         
         // output
         fout << line << "\ntags:\t";
@@ -94,8 +83,6 @@ int main(int argc,char * argv[])
     }
     fin.close();
     fout.close();
-
-    // gt.output_tag_rank("../dict/model.txt");
 }
 
 
